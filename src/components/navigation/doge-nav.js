1ad6164fe53b0f3bebbb117ff0082691ea0a5152
blend <151626101+blendtwenty@users.noreply.github.com>
2024-02-09 21:23:06 +1100

    enhancements to doge-nav, adds mobile support and "controls"

--- src/components/navigation/doge-nav.js
+++ src/components/navigation/doge-nav.js
@@ -1,12 +1,14 @@
 import { dogeComponentInit, resourceBasePath } from "../../lib/cherry.js"
-import { LitElement, css, html } from "../../lib/lit/dist@3/lit-core.min.js";
+import { LitElement, css, html, classMap } from "../../lib/lit/dist@3/lit-all.min.js";
 
 export class DogeNav extends LitElement {
 	static styles = css`
 		:host {
 			width: 100%;
 			display: block;
-			position: relative;
+			position: fixed;
+      top: 0px;
+      z-index: 999;
 
 			/* make themeable */
 			background: var(--doge-black);
@@ -18,10 +20,10 @@ export class DogeNav extends LitElement {
 			max-height: 100px;
 			width: 100%;
 			max-width: 1440px;
+      align-items: center;
 			justify-content: space-between;
-			z-index: 99;
 
-			color: white;
+			color: #a9a9b3;
 		}
 
 		header .logo {
@@ -37,18 +39,13 @@ export class DogeNav extends LitElement {
 			z-index: 99;
 		}
 
-		/* TODO: mobile styles
 		header nav {
-			display: flex;
-			flex-direction: column;
-			padding: 1rem 0;
-			width: 100%;
-		}*/
-
-		header nav {
-			display: flex;
-			align-items: center;
+      display: none;
+      @media only screen and (min-width: 980px) {
+        display: flex;
+      }
 			flex-direction: row;
+      align-items: center;
 			justify-content: flex-end;
 			padding: 0 15px;
 
@@ -58,8 +55,9 @@ export class DogeNav extends LitElement {
 
 		::slotted(a),
 		::slotted(a:visited) {
-			color: white;
+			color: #a9a9b3;
 			text-decoration: none;
+      white-space: nowrap;
 		}
 
 		::slotted(a) {
@@ -68,7 +66,7 @@ export class DogeNav extends LitElement {
 		}
 
 		::slotted(a:hover) {
-			color: var(--doge-yellow);
+			color: #ffffff;
 			text-decoration: underline;
 		}
 
@@ -76,6 +74,10 @@ export class DogeNav extends LitElement {
 			color: var(--doge-yellow);
 			text-decoration: underline;
 		}
+
+    ::slotted(div) {
+      margin: 1px 9px 0px 9px;
+    }
 	`;
 
 	constructor() {
@@ -96,6 +98,7 @@ export class DogeNav extends LitElement {
 				<nav>
 					<slot></slot>
 				</nav>
+        <slot name="mobile"></slot>
 			</header>
 		`;
 	}
@@ -114,14 +117,15 @@ export class DogeNavList extends LitElement {
 			position: absolute;
 			background: var(--doge-black);
 			display: none;
-			left: -9px;
-			padding-top: 4.5px;
-			padding-bottom: 4.5px;
+			right: 0px;
+      text-align: right;
+			padding-top: 18px;
+			padding-bottom: 9px;
 		}
 
 		::slotted(a),
 		::slotted(a:visited) {
-			color: white;
+			color: #a9a9b3;
 			text-decoration: none;
 		}
 
@@ -130,7 +134,7 @@ export class DogeNavList extends LitElement {
 		}
 
 		::slotted(a) {
-			padding: 9px 18px;
+			padding: 4.5px 18px;
 			white-space: nowrap;
 			display: block;
 		}
@@ -140,7 +144,7 @@ export class DogeNavList extends LitElement {
 		}
 
 		::slotted(a:hover) {
-			color: var(--doge-yellow);
+			color: white;
 			text-decoration: underline;
 		}
 
@@ -411,4 +415,115 @@ class DogeSitePickerItem extends LitElement {
   }
 }
 customElements.define("doge-site-picker-item", DogeSitePickerItem);
+
+export class DogeNavMobile extends LitElement {
+  static styles = css`
+    :host {
+      @media only screen and (min-width: 980px) {
+        display: none;
+      }
+
+      display: flex;
+      font-size: 1.1rem;
+      ::slotted(a),
+    }
+    .menu-trigger {
+      @media only screen and (min-width: 980px) {
+        display: none;
+      }
+
+      display: inline-block;
+      margin-right: 15px;
+      fill: currentColor;
+      margin-top: 2px;
+
+      cursor: pointer;
+    }
+
+    nav {
+      @media only screen and (min-width: 980px) {
+        display: none;
+      }
+
+      display: flex;
+      flex-direction: column;
+      text-align: right;
+      font-family: var(--doge-font-fancy);
+      
+      position: absolute;
+      top: 60px;
+      right: 0px;
+      z-index: 99;
+      
+      width: auto;
+      
+      background: black;
+      padding: .5em .7em;
+
+    }
+    ::slotted(a) {
+      padding:.3em;
+    }
+
+    ::slotted(a),
+    ::slotted(a:visited) {
+      color: #a9a9b3;
+      text-decoration: none;
+    }
+
+    ::slotted(a:hover) {
+      color: white;
+      text-decoration: underline;
+    }
+
+    ::slotted(a[active]) {
+      color: var(--doge-yellow);
+      text-decoration: underline;
+    }
+    ::slotted(span) {
+      display: block;
+      margin-top: 1em;
+          color: var(--doge-purple);
+          font-family: sans-serif;
+          font-size: 0.8rem;
+          padding:.3em;
+          padding-bottom: 0em;
+      }
+    .hidden {
+      display: none;
+    }
+  `;
+
+  static properties = {
+    isListHidden: {}
+  }
+
+  constructor() {
+    super();
+    this.isListHidden = true;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+      dogeComponentInit();
+    }
+
+    _handleNavMenuClick() {
+      this.isListHidden = !this.isListHidden;
+    }
+
+  render() {
+    const classes = { hidden: this.isListHidden }
+    return html`
+      <span class="menu-trigger" @click=${this._handleNavMenuClick}>
+        <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M24 18v1H0v-1h24zm0-6v1H0v-1h24zm0-6v1H0V6h24z" fill="#1040e2"></path><path d="M24 19H0v-1h24v1zm0-6H0v-1h24v1zm0-6H0V6h24v1z"></path></svg>
+      </span>
+      <nav class=${classMap(classes)}>
+        <slot name="controls"></slot>
+        <slot></slot>
+      </nav>
+    `;
+  }
+}
+customElements.define("doge-nav-mobile", DogeNavMobile);
 
