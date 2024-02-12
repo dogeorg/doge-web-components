6abaf8811f18427995546ed9cc13e314802ad4b8
blendtwenty <blendtwenty@users.noreply.github.com>
2024-02-12 22:24:18 +0000

    deploy: 944ca3f6febf09c5b94209160a38e07b7040cf7e

---
+++ doge-pal.js
@@ -0,0 +1,48 @@
+import { 
+  dogeComponentInit,
+  resourceBasePath,
+  LitElement, css, html, classMap
+} from "../lib/cherry.js"
+
+export class DogePal extends LitElement {
+	static styles = css``;
+
+	static properties = {
+		imageUrl: {},
+		max_width: { attribute: true },
+		max_height: { attribute: true }
+	};
+
+	constructor() {
+		super();
+		this.imageUrl = undefined;
+		this.max_width = "100%";
+		this.max_height = "600px";
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
+		this.fetchImage();
+	}
+
+	async fetchImage() {
+		try {
+			const response = await fetch("https://dog.ceo/api/breed/shiba/images/random");
+			const data = await response.json();
+			if (data.status !== "success") {
+				throw new Error('unsuccessful response from dog.ceo api')
+			}
+			this.imageUrl = data.message;
+		} catch (error) {
+			console.error("Error fetching random image:", error);
+		}
+	}
+
+	render() {
+		return html`
+			<img src="${this.imageUrl}" style="max-width: ${this.max_width}"; max-height: ${this.max_height}" />
+		`;
+	}
+}
+
+customElements.define("doge-pal", DogePal);
---
+++ doge-price.js
@@ -0,0 +1,47 @@
+import { 
+  dogeComponentInit,
+  resourceBasePath,
+  LitElement, css, html, classMap
+} from "../lib/cherry.js"
+
+export class DogePrice extends LitElement {
+  static styles = css``;
+
+  static properties = {
+    interval: { type: Number },
+    currency: { type: String },
+    price: { type: Number, attribute: true, reflect: true },
+  };
+
+  constructor() {
+    super();
+    this.interval = 300000; // Default interval is 5 minutes
+    this.currency = 'usd'; // Default currency is USD
+    this.price = 0; // Initialize the price property
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.fetchPrice();
+    setInterval(() => this.fetchPrice(), this.interval);
+  }
+
+  async fetchPrice() {
+    try {
+      const response = await fetch(`https://api.coingecko.com/api/v3/simple/price?ids=dogecoin&vs_currencies=${this.currency}`);
+      const data = await response.json();
+      const price = data.dogecoin[this.currency];
+      this.price = price;
+    } catch (error) {
+      console.error('Error fetching Dogecoin price:', error);
+    }
+  }
+
+  render() {
+    return html`
+      <span>${this.price} ${this.currency.toUpperCase()}</span>
+    `;
+  }
+}
+
+customElements.define('doge-price', DogePrice);
