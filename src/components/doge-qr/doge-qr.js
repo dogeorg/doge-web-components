d1af1c5362dd57845a57ffe102a6dff64114eacb
blend <151626101+blendtwenty@users.noreply.github.com>
2024-02-26 22:01:32 +1100

    adds some predefined themes and beginning of ability to customise

--- src/components/doge-qr/doge-qr.js
+++ src/components/doge-qr/doge-qr.js
@@ -8,39 +8,116 @@ import "../../../resources/blocks/qr-code-styling@1.5.0/qr-code-styling.js";
 export class DogeQR extends LitElement {
 	static properties = {
 		address: {},
-		logo: {},
+    caption: {},
+    colorBg: {},
+    colorFg: {},
+    noBorder: { type: Boolean },
+    noLogo: { type: Boolean },
+    size: {},
+    theme: {},
 		qrCanvas: {},
-		caption: {},
 	};
+  static sizes = {
+    sm: 100,
+    md: 200,
+    lg: 300,
+    xl: 400,
+  }
+  static themes = {
+    'such-doge': {
+      colorFg: "#de9a1e, #ffc107, #de9a1e",
+      colorBg: "white",
+      image: `https://fetch.dogecoin.test/resources/img/qr/such-cool-2.png`,
+    },
+    'much-dev': {
+      colorFg: "#7020c8, #02f8da, #312147",
+      colorBg: "white",
+      image: `${resourceBasePath}/img/logos/circle/logo-circle-developers.png`,
+    },
+    'so-coin': {
+      colorFg: "#ac8639, #ceab4b, #755b35",
+      colorBg: "white",
+      image: `${resourceBasePath}/img/logos/circle/logo-circle-dogecoin.png`,
+    },
+    'very-community': {
+      colorFg: "#7e3357, #f44e9f, #7e3357",
+      colorBg: "white",
+      image: `https://fetch.dogecoin.org/resources/img/logos/circle/logo-circle-community.png`,
+    }
+  }
+
+  static styles = css`
+    .container {
+      display: inline-block;
+      padding: 0.3em;
+    }
+    .container[noborder="true"] {
+      padding: 0 !important;
+      background: red;
+    }
+  `;
 
 	constructor() {
 		super();
-		this.address = "";
-		this.logo = true;
+    this.colorFg = "#ffc107";
+    this.colorBg = "white";
+    this.size = "md";
 		this.qrCanvas
-		this.caption = ""
 	}
 
+  generateColorStops(colors) {
+    const colorArray = colors.split(',').map(c => c.trim());
+    return colorArray.map((c, index) => ({
+      offset: index,
+      color: c,
+    }));
+  }
+
+  applyTheme() {
+    if (!this.theme) {
+      return {
+        backgroundOptions: {
+          color: this.colorBg
+        },
+        dotsOptions: {
+          gradient: {
+            type: "linear",
+            colorStops: this.generateColorStops(this.colorFg)
+          }
+        }
+      }
+    }
+
+    return {
+      image: this.noLogo ? '' : DogeQR.themes[this.theme].image,
+      backgroundOptions: {
+        color: DogeQR.themes[this.theme].colorBg
+      },
+      dotsOptions: {
+        gradient: {
+          type: "linear",
+          colorStops: this.generateColorStops(DogeQR.themes[this.theme].colorFg)
+        }
+      }
+    }
+  }
+
 	firstUpdated() {
-  		this.qrCanvas = this.renderRoot.querySelector('#qrCanvas');
+		this.qrCanvas = this.renderRoot.querySelector('#qrCanvas');
 
  		try {
 			const qrCode = new QRCodeStyling({
-				width: 220,
-				height: 220,
+				width: DogeQR.sizes[this.size],
+				height: DogeQR.sizes[this.size],
 				type: "svg",
 				data: this.address,
-				image: `${resourceBasePath}/img/qr/shiba-sm.png`,
-				dotsOptions: {
-					color: "var(--doge-yellow)",
-				},
-				backgroundOptions: {
-					color: "",
-				},
-				imageOptions: {
-					crossOrigin: "anonymous",
-					imageSize: 0.4
-				},
+        imageOptions: {
+          crossOrigin: "anonymous",
+          hideBackgroundDots: false,
+          imageSize: 0.5,
+          margin: 0
+        },
+        ...this.applyTheme(),
 			});
 
 			qrCode.append(this.qrCanvas);
@@ -50,6 +127,13 @@ export class DogeQR extends LitElement {
 		}
 	}
 
+  getBackroundColor() {
+    if (this.theme) {
+      return DogeQR.themes[this.theme].colorBg
+    }
+    return '';
+  }
+
 	connectedCallback() {
 		super.connectedCallback();
 		dogeComponentInit();
@@ -57,7 +141,7 @@ export class DogeQR extends LitElement {
 
 	render() {
 		return html`
-			<div class="container">
+			<div class="container" noborder=${this.noBorder} style="background-color: ${this.getBackroundColor()}">
 				<div id="qrCanvas"></div>
 				${this.caption
 					? html`<span class="caption">${this.caption}</span>`
